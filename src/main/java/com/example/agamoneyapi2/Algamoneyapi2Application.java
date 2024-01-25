b417a9ea14e55d1a68fffffd1ecf7f5177faabbc
nelson <nelson@NELSON-DESKTOP>
2024-01-25 16:48:45 -0300

    Criando Projeto STS

---
+++ src/main/java/com/example/agamoneyapi2/Algamoneyapi2Application.java
@@ -0,0 +1,13 @@
+package com.example.agamoneyapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Algamoneyapi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Algamoneyapi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/agamoneyapi2/Algamoneyapi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agamoneyapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Algamoneyapi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
