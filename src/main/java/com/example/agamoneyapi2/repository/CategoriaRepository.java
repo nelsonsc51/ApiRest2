7695ff3a8c66293d9f369db2f17ae650b3f9beb0
nelson <nelson@NELSON-DESKTOP>
2024-01-25 18:07:20 -0300

    Criado a interface CategoriaRepository

---
+++ src/main/java/com/example/agamoneyapi2/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.example.agamoneyapi2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.agamoneyapi2.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+}
