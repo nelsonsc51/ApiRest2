0abb236983ffba1446e66e166d3bb0a55a295f56
nelson <nelson@NELSON-DESKTOP>
2024-02-07 18:43:55 -0300

    3.7 - Retornando 404 not found ao dar GETMapping

--- src/main/java/com/example/agamoneyapi2/resource/CategoriaResource.java
+++ src/main/java/com/example/agamoneyapi2/resource/CategoriaResource.java
@@ -1,25 +1,23 @@
 package com.example.agamoneyapi2.resource;
 
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.agamoneyapi2.model.Categoria;
 import com.example.agamoneyapi2.repository.CategoriaRepository;
 
 import jakarta.servlet.http.HttpServletResponse;
-
-import com.example.agamoneyapi2.model.Categoria;
-
-import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
@@ -44,7 +42,8 @@ public class CategoriaResource {
 	}
 	
 	@GetMapping("/{codigo}")
-	public Categoria buscarPeloCodigo(@PathVariable Long codigo){
-		return categoriaRepository.findById(codigo).orElse(null);
+	public ResponseEntity<Categoria>  buscarPeloCodigo(@PathVariable Long codigo){
+		Optional<Categoria> categoria = this.categoriaRepository.findById(codigo);
+		return categoria.isPresent() ? ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
 	}
 }
