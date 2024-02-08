8710f7602861e0a03a15c16da67dcc3866e05730
nelson <nelson@NELSON-DESKTOP>
2024-02-08 15:58:30 -0300

    3.9 - ratando erros com ExceptionHandler

--- src/main/java/com/example/agamoneyapi2/Algamoneyapi2Application.java
+++ src/main/java/com/example/agamoneyapi2/Algamoneyapi2Application.java
@@ -2,6 +2,8 @@ package com.example.agamoneyapi2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class Algamoneyapi2Application {
@@ -9,5 +11,13 @@ public class Algamoneyapi2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Algamoneyapi2Application.class, args);
 	}
+	
+	@Bean(name="messageSource")
+    public ResourceBundleMessageSource bundleMessageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+    return messageSource;
+    }
 
 }
